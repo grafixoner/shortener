c0e7e4e8e7749888ae2379561c591c4497b2178a
Michael Bailey <grafixoner@gmail.com>
2021-07-29 11:14:20 -0600

    Update shorten.py

--- modules/shorten.py
+++ modules/shorten.py
@@ -1,3 +1,6 @@
+from math import floor
+import string
+
 class ListMaster():
     def __init__(self):
         return
@@ -5,5 +8,20 @@ class ListMaster():
     def expand(self):
         print ('in here')
 
-    def shorten(self, web, request):  
+    def shorten(self, web, request):
+        ''' Normally we would just use an existing library hashlib is a good one.
+            But we are going to roll our own using base62 and base10
+        '''
+
         print ('in here')
+
+
+    def base62(salt, x = 62):
+        base = string.digits + string.lowercase + string.uppercase
+
+        return base
+
+    def base10(salt, x = 62):
+        base = string.digits + string.lowercase + string.uppercase
+
+        return base
